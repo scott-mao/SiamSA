de5f210b74fddaac22e0fde513ca0a3ef638fdd3
Guangze Zheng <57896775+George-Zhuang@users.noreply.github.com>
2021-09-17 21:38:34 +0800

    first commit

---
+++ toolkit/datasets/__init__.py
@@ -0,0 +1,21 @@
+from .uav10fps import UAV10Dataset
+from .uav20l import UAV20Dataset
+from .uamt100 import UAMT100
+class DatasetFactory(object):
+    @staticmethod
+    def create_dataset(**kwargs):
+
+
+        assert 'name' in kwargs, "should provide dataset name"
+        name = kwargs['name']
+        
+        if 'UAV123_10fps' in name:
+            dataset = UAV10Dataset(**kwargs)
+        elif 'UAV20L' in name:
+            dataset = UAV20Dataset(**kwargs)
+        elif 'UAMT100' in name:
+            dataset = UAMT100(**kwargs)
+        else:
+            raise Exception("unknow dataset {}".format(kwargs['name']))
+        return dataset
+
